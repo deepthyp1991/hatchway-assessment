322b8f1969e6c9ec35058529a264238c621cc9d5
deepthy <deepthyp1991@gmail.com>
2021-04-09 14:10:36 -0400

    styled the web page

--- src/App.js
+++ src/App.js
@@ -4,56 +4,56 @@ import Student from "./components/Student"
 import './App.css';
 
 function App() {
-  const [info, setInfo] = useState([])
+const [info, setInfo] = useState([])
 
-  useEffect(() => {
-    getStudents()
-  }, [])
+useEffect(() => {
+  getStudents()
+}, [])
 
-  const getStudents = async () => {
-    const response = await fetch(
-      `https://www.hatchways.io/api/assessment/students`
-    );
-    const data = await response.json();
-    console.log(data)
-    setInfo(data.students)
-
-  }
-  return (
-    <div className="App">
-    <form>
-      <input className ="serach-bar" type="text"/>
-      <button className= "search-button" type="submit">
-        serach
-      </button>
-    </form>
-    {info.map(stud => {
-      function findAverage(array) {
-        let sum = 0;
-        for(let i =0; i < array.length; i++) {
-          sum += parseInt(array[i])
-        }
-        let average = sum/array.length;
-        return average;
-      }
-       const averageGrade = findAverage(stud.grades)
-      return (
-      <Student 
-      key = {stud.id}
-      image = {stud.pic}
-      Email = {stud.email}
-      FirstName = {stud.firstName}
-      Skill = {stud.skill}
-      Company = {stud.company}
-      Average = {averageGrade}
-      />
-    );
-})}
-   
-  
-    
-    </div>
+const getStudents = async () => {
+  const response = await fetch(
+    `https://www.hatchways.io/api/assessment/students`
   );
+  const data = await response.json();
+  console.log(data)
+  setInfo(data.students)
+
+}
+return (
+  <div className="App">
+    <div className= "contentContainer">
+    <form>
+    <input className ="serach-bar" type="text"/>
+    <button className= "search-button" type="submit">
+      serach
+    </button>
+  </form>
+  {info.map(stud => {
+    function findAverage(array) {
+      let sum = 0;
+      for(let i =0; i < array.length; i++) {
+        sum += parseInt(array[i])
+      }
+      let average = sum/array.length;
+      return average;
+    }
+    const averageGrade = findAverage(stud.grades)
+    return (
+    <Student 
+    key = {stud.id}
+    image = {stud.pic}
+    Email = {stud.email}
+    FirstName = {stud.firstName}
+    LastName = {stud.lastName}
+    Skill = {stud.skill}
+    Company = {stud.company}
+    Average = {averageGrade}
+    />
+  );
+})}
+</div>
+</div>
+);
 }
 
 export default App;
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import "./Student.css";
 
-const Student = ({image, FirstName, Skill, Company, Email, Average}) => {
+const Student = ({image, FirstName, LastName, Skill, Company, Email, Average}) => {
 
   return (
-    <div>
-      <img src={image} alt=""/>
-      <h4>{FirstName}</h4>
+    <div className = "container">
+      <img className = "avatar" src={image} alt=""/>
+      <h1 className = "name">{`${FirstName.toUpperCase()} ${LastName.toUpperCase()}`}</h1>
+      <div className = "content"> 
       <p>Email: {Email}</p>
       <p>Skill: {Skill}</p>
       <p>Comapny: {Company}</p>
-      <p>Avg: {Average}</p>
+      <p>Avg: {Average}%</p>
+      </div>
     </div>
   )
 }
